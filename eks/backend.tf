4de74303ab886c15748d2bd7a8e6bc3eedef1d0e
avinashkalpande <161586365+avinashkalpande@users.noreply.github.com>
2024-09-23 16:04:43 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "devopsified-bucket1"
+    bucket         = "terraform-bucket-mern"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "dynamobd-state-lock"
     encrypt        = true
   }
 }
